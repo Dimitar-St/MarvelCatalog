0567b6d5cef0f5f9ef4e557085069dff3b86875f
Dimitar <stoyanov.dimi.tri@gmail.com>
2017-10-01 14:31:32 -0200

    I added new Area which is for admin.

---
+++ MarvelCatalog-App/MarvelCatalog-App/Areas/Admin/Controllers/CharactersController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MarvelCatalog_App.Areas.Admin.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class CharactersController : Controller
+    {
+        public CharactersController() { }
+
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+        
+    }
+}

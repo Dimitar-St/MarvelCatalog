7e2ff29a0a3c7703067a3ff5dbb5492741aa3d5e
Dimitar <stoyanov.dimi.tri@gmail.com>
2017-10-01 11:20:02 -0200

    I added Nunit.

--- MarvelCatalog-App/MarvelCatalog-App.Tests/Services/CharacterService/Constructor_Should.cs
+++ MarvelCatalog-App/MarvelCatalog-App.Tests/Services/CharacterService/Constructor_Should.cs
@@ -1,7 +1,7 @@
 ﻿using Marvel_Catalog_App.Data.API.Models;
 using MarvelCatalog_App.Data.Repositories;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,10 +10,10 @@ using System.Threading.Tasks;
 
 namespace MarvelCatalog_App.Tests.Services.CharacterService
 {
-    [TestClass]
+    [TestFixture]
     public class Constructor_Should
     {
-        [TestMethod]
+        [Test]
         public void Throw_ArgumentNullException_When_IEfRepository_IsNull()
         {
             var mockEfRepository = new Mock<IEfRepository<CharacterDataModel>>();

57bf3910850ba5925e06ad627a8413a7d8f8c343
Dimitar <stoyanov.dimi.tri@gmail.com>
2017-10-03 11:03:37 -0200

    I added new unit tests for the ComicsController.

---
+++ MarvelCatalog-App/MarvelCatalog-App.Tests/Controllers/ComicsControllerTests/MainComicsPage_Should.cs
@@ -0,0 +1,61 @@
+﻿using AutoMapper;
+using Marvel_Catalog_App.Data.Models;
+using MarvelCatalog_App.Controllers;
+using MarvelCatalog_App.Services.Contracts;
+using MarvelCatalog_App.ViewModels;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarvelCatalog_App.Tests.Controllers.ComicsControllerTests
+{
+    [TestFixture]
+    public class MainComicsPage_Should
+    {
+        [Test]
+        public void Call_GetComicsMethod_FromTheService()
+        {
+            // Arrange
+            var comicsDataModel = new List<ComicsDataModel>();
+            var comicsViewModel = new List<ComicsViewModel>();
+
+            var mockedService = new Mock<IComicsService>();
+            var mockedMapper = new Mock<IMapper>();
+
+            mockedService.Setup(s => s.GetComics()).Returns(comicsDataModel);
+            mockedMapper.Setup(mapper => mapper.Map<IEnumerable<ComicsViewModel>>(comicsDataModel))
+                        .Returns(comicsViewModel);
+
+            var comicsController = new ComicsController(mockedService.Object, mockedMapper.Object);
+
+            comicsController.MainComicsPage();
+
+            mockedService.Verify(s => s.GetComics(), Times.Once);
+        }
+
+        [Test]
+        public void Call_MapMethod_FromTheService()
+        {
+            // Arrange
+            var comicsDataModel = new List<ComicsDataModel>();
+            var comicsViewModel = new List<ComicsViewModel>();
+
+            var mockedService = new Mock<IComicsService>();
+            var mockedMapper = new Mock<IMapper>();
+
+            mockedService.Setup(s => s.GetComics()).Returns(comicsDataModel);
+            mockedMapper.Setup(mapper => mapper.Map<IEnumerable<ComicsViewModel>>(comicsDataModel))
+                        .Returns(comicsViewModel);
+
+            var comicsController = new ComicsController(mockedService.Object, mockedMapper.Object);
+
+            comicsController.MainComicsPage();
+
+            mockedMapper.Verify(mapper => mapper.Map<IEnumerable<ComicsViewModel>>(comicsDataModel), Times.Once);
+        }
+    }
+}

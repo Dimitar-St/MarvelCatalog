7d563ccca0bac176685368036582620a7062184c
Dimitar <stoyanov.dimi.tri@gmail.com>
2017-10-03 14:23:07 -0200

    added new unit test for the comics service

---
+++ MarvelCatalog-App/MarvelCatalog-App.Tests/Services/ComicsServiceTests/Constructor_Should.cs
@@ -0,0 +1,40 @@
+﻿using Marvel_Catalog_App.Data.Models;
+using MarvelCatalog_App.Data.Repositories;
+using MarvelCatalog_App.Data.UnitOfWork;
+using MarvelCatalog_App.Services;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarvelCatalog_App.Tests.Services.ComicsServiceTests
+{
+    [TestFixture]
+    public class Constructor_Should
+    {
+        [Test]
+        public void Throw_ArgumentNullException_When_UnitOfWork_IsNull()
+        {
+            // Arrang & Act & Assert
+            Assert.That(() => new ComicsService(null),
+                        Throws.ArgumentNullException.With.Message.Contains("unitOfWork"));
+        }
+
+        [Test]
+        public void Throw_ArgumentNullException_When_IEfRepository_IsNull()
+        {
+            // Arrange
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            IEfRepository<ComicsDataModel> invalidRepo = null;
+
+            mockedUnitOfWork.SetupGet(unit => unit.ComicsRepository).Returns(invalidRepo);
+
+            // Act & Assert
+            Assert.That(() => new ComicsService(mockedUnitOfWork.Object),
+                                Throws.ArgumentNullException.With.Message.Contains("unitOfWork"));
+        }
+    }
+}

f8c9470ffb3513d7818814a17225d62154d59e28
Dimitar <stoyanov.dimi.tri@gmail.com>
2017-10-05 00:34:43 -0200

    I added new unit tests for CharacterService.

---
+++ MarvelCatalog-App/MarvelCatalog-App.Tests/Services/CharacterServiceTests/GetAllCharactersAdministration_Should.cs
@@ -0,0 +1,63 @@
+﻿using Marvel_Catalog_App.Data.Models;
+using MarvelCatalog_App.Data.Repositories;
+using MarvelCatalog_App.Data.UnitOfWork;
+using MarvelCatalog_App.Services;
+using Moq;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MarvelCatalog_App.Tests.Services.CharacterServiceTests
+{
+    [TestFixture]
+    public class GetAllCharactersAdministration_Should
+    {
+        [Test]
+        public void Call_TheAll_Property_OfTheEfRepository()
+        {
+            // Arrange
+            var characters = new List<CharacterDataModel>()
+            {
+                new CharacterDataModel()
+            };
+
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedRepo = new Mock<IEfRepository<CharacterDataModel>>();
+
+            mockedUnitOfWork.Setup(unit => unit.CharactersRepository).Returns(mockedRepo.Object);
+            mockedRepo.Setup(repo => repo.All).Returns(characters);
+
+            var characterService = new CharacterService(mockedUnitOfWork.Object);
+
+            // Act
+            characterService.GetAllCharactersAdministration();
+
+            // Assert
+            mockedRepo.Verify(repo => repo.All, Times.Once);
+        }
+
+        [Test]
+        public void Return_ACharactersDataModels()
+        {
+            // Arrange
+            var characters = new List<CharacterDataModel>()
+            {
+                new CharacterDataModel()
+            };
+
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedRepo = new Mock<IEfRepository<CharacterDataModel>>();
+
+            mockedUnitOfWork.Setup(unit => unit.CharactersRepository).Returns(mockedRepo.Object);
+            mockedRepo.Setup(repo => repo.All).Returns(characters);
+
+            var characterService = new CharacterService(mockedUnitOfWork.Object);
+
+            // Act
+            var charactersData = characterService.GetAllCharactersAdministration();
+
+            // Assert
+            Assert.IsInstanceOf<CharacterDataModel>(charactersData.First());
+        }
+    }
+}
---
+++ MarvelCatalog-App/MarvelCatalog-App.Tests/Services/CharacterServiceTests/GetCharacterById_Should.cs
@@ -0,0 +1,76 @@
+﻿using Marvel_Catalog_App.Data.Models;
+using MarvelCatalog_App.Data.Repositories;
+using MarvelCatalog_App.Data.UnitOfWork;
+using MarvelCatalog_App.Services;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarvelCatalog_App.Tests.Services.CharacterServiceTests
+{
+    [TestFixture]
+    public class GetCharacterById_Should
+    {
+        [Test]
+        public void Call_AllProperty_FromEfRepository()
+        {
+            // Arrange
+            var characters = new List<CharacterDataModel>()
+            {
+                new CharacterDataModel()
+            };
+
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedRepo = new Mock<IEfRepository<CharacterDataModel>>();
+
+            mockedUnitOfWork.Setup(unit => unit.CharactersRepository).Returns(mockedRepo.Object);
+            mockedRepo.Setup(repo => repo.All).Returns(characters);
+
+            var characterService = new CharacterService(mockedUnitOfWork.Object);
+
+            // Act
+            characterService.GetCharacterById(1);
+
+            // Assert
+            mockedRepo.Verify(repo => repo.All, Times.Once);
+        }
+
+        [Test]
+        public void Return_ACharacterModel()
+        {
+            // Arrange
+            var characters = new List<CharacterDataModel>()
+            {
+                new CharacterDataModel()
+                {
+                    Id = 1,
+                    Name = "Mitko"
+                },
+                new CharacterDataModel()
+                {
+                    Id = 123,
+                    Name = "Niki"
+                }
+            };
+
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+            var mockedRepo = new Mock<IEfRepository<CharacterDataModel>>();
+            var expected = characters[1]; 
+
+            mockedUnitOfWork.Setup(unit => unit.CharactersRepository).Returns(mockedRepo.Object);
+            mockedRepo.Setup(repo => repo.All).Returns(characters);
+
+            var characterService = new CharacterService(mockedUnitOfWork.Object);
+
+            // Act
+            var character = characterService.GetCharacterById(123);
+
+            // Assert
+            Assert.AreEqual(expected.Name, character.Name);
+        }
+    }
+}

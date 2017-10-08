dcd71c2ec7ccf36f1ce9d5815fe564aa142dbe36
Dimitar <stoyanov.dimi.tri@gmail.com>
2017-10-08 11:55:05 -0200

    I added the files for the unit tests.

---
+++ MarvelCatalog-App/MarvelCatalog-App.Tests/Admin/ControllersTest/CharactersAdministrationControllerTests/GetAllCharacters_Should.cs
@@ -0,0 +1,68 @@
+﻿using AutoMapper;
+using Marvel_Catalog_App.Data.Models;
+using MarvelCatalog_App.Areas.Admin.Controllers;
+using MarvelCatalog_App.Areas.Admin.Models;
+using MarvelCatalog_App.Services.Contracts;
+using MarvelCatalog_App.ViewModels;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarvelCatalog_App.Tests.Admin.ControllersTest.CharactersAdministrationControllerTests
+{
+    [TestFixture]
+    public class GetAllCharacters_Should
+    {
+        [Test]
+        public void Call_GetAllCharactersAdministrationMethod_FromTheService()
+        {
+            // Arrange
+            var charactersDataModels = new List<CharacterDataModel>();
+            var charactersVeiwModel = new List<CharactersAdminViewModel>();
+
+            var mockedService = new Mock<ICharacterService>();
+            var mockedMapper = new Mock<IMapper>();
+
+            mockedService.Setup(service => service.GetAllCharactersAdministration())
+                         .Returns(charactersDataModels);
+            mockedMapper.Setup(mapper => mapper.Map<IEnumerable<CharactersAdminViewModel>>(charactersDataModels))
+                        .Returns(charactersVeiwModel);
+
+            var characterAdminController = new CharactersAdministrationController(mockedService.Object, mockedMapper.Object);
+
+            // Act
+            characterAdminController.GetAllCharacters();
+
+            // Assert
+            mockedService.Verify(service => service.GetAllCharactersAdministration(), Times.Once);
+        }
+
+        [Test]
+        public void Call_MapMethod_FromTheMapper()
+        {
+            // Arrange
+            var charactersDataModels = new List<CharacterDataModel>();
+            var charactersVeiwModel = new List<CharactersAdminViewModel>();
+
+            var mockedService = new Mock<ICharacterService>();
+            var mockedMapper = new Mock<IMapper>();
+
+            mockedService.Setup(service => service.GetAllCharactersAdministration())
+                         .Returns(charactersDataModels);
+            mockedMapper.Setup(mapper => mapper.Map<IEnumerable<CharactersAdminViewModel>>(charactersDataModels))
+                        .Returns(charactersVeiwModel);
+
+            var characterAdminController = new CharactersAdministrationController(mockedService.Object, mockedMapper.Object);
+
+            // Act
+            characterAdminController.GetAllCharacters();
+
+            // Assert
+            mockedMapper.Verify(mapper => mapper.Map<IEnumerable<CharactersAdminViewModel>>(charactersDataModels), Times.Once);
+        }
+    }
+}
---
+++ MarvelCatalog-App/MarvelCatalog-App.Tests/Admin/ControllersTest/CharactersAdministrationControllerTests/GetCharacterById_Should.cs
@@ -0,0 +1,68 @@
+﻿using AutoMapper;
+using Marvel_Catalog_App.Data.Models;
+using MarvelCatalog_App.Areas.Admin.Controllers;
+using MarvelCatalog_App.Areas.Admin.Models;
+using MarvelCatalog_App.Services.Contracts;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarvelCatalog_App.Tests.Admin.ControllersTest.CharactersAdministrationControllerTests
+{
+    [TestFixture]
+    public class GetCharacterById_Should
+    {
+        [Test]
+        public void Call_GetCharacterByIdMethod_FromTheService()
+        {
+            // Arrange
+            var characterDataModel = new CharacterDataModel();
+            var characterVeiwModel = new CharactersAdminViewModel();
+
+            var mockedService = new Mock<ICharacterService>();
+            var mockedMapper = new Mock<IMapper>();
+
+            mockedService.Setup(service => service.GetCharacterById(1))
+                         .Returns(characterDataModel);
+            mockedMapper.Setup(mapper => mapper.Map<CharactersAdminViewModel>(characterVeiwModel))
+                        .Returns(characterVeiwModel);
+
+            var characterAdminController = new CharactersAdministrationController(mockedService.Object, mockedMapper.Object);
+
+            // Act
+            characterAdminController.GetCharacterById(1);
+
+
+            // Arrange
+            mockedService.Verify(service => service.GetCharacterById(1), Times.Once);
+        }
+
+        [Test]
+        public void Call_MapMethod_FromTheMapper()
+        {
+            // Arrange
+            var characterDataModel = new CharacterDataModel();
+            var characterVeiwModel = new CharactersAdminViewModel();
+
+            var mockedService = new Mock<ICharacterService>();
+            var mockedMapper = new Mock<IMapper>();
+
+            mockedService.Setup(service => service.GetCharacterById(1))
+                         .Returns(characterDataModel);
+            mockedMapper.Setup(mapper => mapper.Map<CharactersAdminViewModel>(characterVeiwModel))
+                        .Returns(characterVeiwModel);
+
+            var characterAdminController = new CharactersAdministrationController(mockedService.Object, mockedMapper.Object);
+
+            // Act
+            characterAdminController.GetCharacterById(1);
+            
+            // Arrange
+            mockedMapper.Verify(mapper => mapper.Map<CharactersAdminViewModel>(characterDataModel), Times.Once);
+        }
+    }
+}

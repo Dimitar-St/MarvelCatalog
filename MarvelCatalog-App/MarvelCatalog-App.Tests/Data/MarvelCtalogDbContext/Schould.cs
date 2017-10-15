073f60d80b10274921bf566fc027a9d6e296f99d
Dimitar <stoyanov.dimi.tri@gmail.com>
2017-10-15 16:18:47 -0200

    I added new unit tests for the DbContext.

---
+++ MarvelCatalog-App/MarvelCatalog-App.Tests/Data/MarvelCtalogDbContext/Schould.cs
@@ -0,0 +1,35 @@
+﻿using MarvelCatalog_App.Data;
+using MarvelCatalog_App.Data.Contracts;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarvelCatalog_App.Tests.Data.MarvelCtalogDbContext
+{
+    [TestFixture]
+    public class Schould
+    {
+        [Test]
+        public void CreateInstanceOfDatabase()
+        {
+            // Arrange
+            var db = new EfMarvelCatalogDbContext();
+
+            // Act & Assert
+            Assert.IsInstanceOf<EfMarvelCatalogDbContext>(db);
+        }
+
+        [Test]
+        public void BeTypeOfICarsDbContext()
+        {
+            // Arrange
+            var db = new EfMarvelCatalogDbContext();
+
+            // Act & Assert
+            Assert.IsInstanceOf<IEfMarvelCatalogDbContext>(db);
+        }
+    }
+}
